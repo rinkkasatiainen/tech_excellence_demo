c087147723ea6c68a29f0c88c77c3cb0a507f4e1
Aki Salmi <aki.salmi@iki.fi>
2017-10-16 11:52:25 +0300

    introducing EventPublisher, next inline method

--- src/main/java/com/example/fi/rinkkasatiainen/model/EventStore.java
+++ src/main/java/com/example/fi/rinkkasatiainen/model/EventStore.java
@@ -14,9 +14,24 @@ public interface EventStore {
     void saveEvents(UUID random, List<Event> uncommittedChanges, Integer lastVersion);
 
     default void save(FeedbackerUUID feedbackerUUID, AggregateRoot session, Integer expectedVersion) {
-        List<Event> uncommittedChanges = session.getUncommittedChanges();
-        saveEvents(feedbackerUUID.getId(), uncommittedChanges, expectedVersion);
-        session.markChangesAsCommitted();
+        new EventPublisher(this).save(feedbackerUUID, session, expectedVersion);
+//        List<Event> uncommittedChanges = session.getUncommittedChanges();
+//        saveEvents(feedbackerUUID.getId(), uncommittedChanges, expectedVersion);
+//        session.markChangesAsCommitted();
 
+    }
+
+    class EventPublisher{
+        private final EventStore eventStore;
+
+        public EventPublisher(EventStore eventStore) {
+            this.eventStore = eventStore;
+        }
+
+        void save(FeedbackerUUID feedbackerUUID, AggregateRoot session, Integer expectedVersion) {
+            List<Event> uncommittedChanges = session.getUncommittedChanges();
+            eventStore.saveEvents(feedbackerUUID.getId(), uncommittedChanges, expectedVersion);
+            session.markChangesAsCommitted();
+        }
     }
 }
